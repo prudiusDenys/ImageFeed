8dae0eb3d8d1cca3ea6be8559e8fbdab36f5fd7d
Denys Prudius <prdslife@gmail.com>
2025-06-19 19:56:45 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Denys Prudius on 19.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
