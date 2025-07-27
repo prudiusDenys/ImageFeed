9c150be81f285cdc32caefbe93e17599df7e949b
Denis Prudius <prdslife@gmail.com>
2025-07-27 14:38:47 +0300

    Удалил лишние отступы

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -5,8 +5,6 @@ final class ImagesListViewController: UIViewController {
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
-    
-    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
