9efd136590acb7979d0caa25739cbf5bbb026d79
Denis Prudius <prdslife@gmail.com>
2025-08-15 18:44:08 +0300

    sprint 10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "s_g949JJedpXlkQRhKKPygVk80ZFHMSAXfQw3S1a37I"
+    static let secretKey = "Q3OhDUEBr9Wr29W5VhzSfqpei1Q8XtTCtEsg0sDD9wg"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
